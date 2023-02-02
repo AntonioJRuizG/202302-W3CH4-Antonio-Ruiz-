4ac9e50ed8c9a29933edbec70840417360c6d8d4
AntonioJesusRG <antoniogar129@gmail.com>
2023-02-02 20:58:41 +0100

    Delete links in index of delete files

--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,10 @@
 import { Header } from './components/header/header';
 import { PendingSeries } from './components/pendingSeriesSection/pendingSeries';
 import { SeriesSection } from './components/seriesSection/seriesSection';
-import { Tasks } from './components/tasks/tasks';
 import { WatchedSeries } from './components/watchedSeriesSection/watchedSeries';
-import { TaskStorageRepo } from './services/repository/task.storage.repo';
 
 console.log('Load', location.pathname);
 new Header('.container');
 new SeriesSection('.main');
 new PendingSeries('.series');
 new WatchedSeries('.series');
-if (location.pathname === '/home') {
-  new Tasks('main', new TaskStorageRepo());
-}
